9c020598c3a383310067848f45e91aea630d1f02
Shinichi <miyakawashi@gmail.com>
2021-12-07 21:33:05 -0500

    dynamic removal of five-day forecast and current forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,10 @@
 var formEl = document.getElementById("city-search");
 var currentForecastEl = document.getElementById("current-forecast");
 var fiveDayForecastEl = document.getElementById("five-day-forecast");
-var currentDate = moment().format("MM/DD/YYYY");
+var searchButtonEl  = document.getElementById("city-search");
+var cityTextEl = document.getElementById("city");
 
+var currentDate = moment().format("MM/DD/YYYY");
 var apiKey = "08de60513f8aaba79d4697d19640606f";
 
 // find maximum temperature of time block and return its index
@@ -19,7 +21,8 @@ var findMaxTemp = function(arr){
     return maxTempIndex;
 };
 
-var findDateArr = function(arr) {
+// finds the ending index of each dat for the 5 forecast 
+var findDateArr = function(array) {
     var fiveDayArr = [
         moment().add(1, 'days').format("YYYY-MM-DD"),
         moment().add(2, 'days').format("YYYY-MM-DD"),
@@ -30,17 +33,26 @@ var findDateArr = function(arr) {
 
     var dateIndex = new Array(5).fill(0);
     
-    for (let i=0; i<arr.length; i++) {
+    for (let i=0; i<array.length; i++) {
 
-        if (arr[i].dt_txt.split(" ")[0] === fiveDayArr[0]) {
+        if (array[i].dt_txt.split(" ")[0] === fiveDayArr[0]) {
+
             dateIndex[0] = i;
-        } else if (arr[i].dt_txt.split(" ")[0] === fiveDayArr[1]) {
+
+        } else if (array[i].dt_txt.split(" ")[0] === fiveDayArr[1]) {
+
             dateIndex[1] = i;
-        } else if (arr[i].dt_txt.split(" ")[0] === fiveDayArr[2]) {
+
+        } else if (array[i].dt_txt.split(" ")[0] === fiveDayArr[2]) {
+
             dateIndex[2] = i;
-        } else if (arr[i].dt_txt.split(" ")[0] === fiveDayArr[3]) {
+
+        } else if (array[i].dt_txt.split(" ")[0] === fiveDayArr[3]) {
+
             dateIndex[3] = i;
+
         } else {
+
             dateIndex[4] = i;
         }
     }
@@ -50,34 +62,45 @@ var findDateArr = function(arr) {
 // Locate 5 day forecast from weather object
 var parseForecast = function(obj){
 
+    // obj.list returns large array of 5 day forecasts with 3 hour time blocks.
     var futureForecastArr = obj.list;
-    var dateIndex = findDateArr(futureForecastArr);
 
+    // Get ending index for each day for the five day forecast.
+    var datesLastIndex = findDateArr(futureForecastArr);
     var maxTempArr = new Array();
+
+    // variable to store starting index to slice weather list from object.
     var start = 0;
-    for (let i=0; i<dateIndex.length; i++) {
+
+    // loop through array containing the ending indices for each day (5 day sub-set of futureForecastArr)
+    for (let i=0; i<datesLastIndex.length; i++) {
+        // If it is the first day
         if (i === 0) {
-            maxTempIndex = findMaxTemp(futureForecastArr.slice(start, dateIndex[i]+1));
+            maxTempIndex = findMaxTemp(futureForecastArr.slice(start, datesLastIndex[i]+1));
             maxTempArr
             .push(
-                {
+                {   // dt_txt attribute for date, returns string
                     date: moment(futureForecastArr[maxTempIndex].dt_txt).format("M/DD/YYYY"),
                     temp: futureForecastArr[maxTempIndex].main.temp,
                     wind: futureForecastArr[maxTempIndex].wind.speed,
                     humidity: futureForecastArr[maxTempIndex].main.humidity
                 });
         } else {
-            maxTempIndex = findMaxTemp(futureForecastArr.slice(start, dateIndex[i]+1)) + dateIndex[i-1];
+            // slice will return an array from [start, index - 1]
+            // Add last index of array to index, for grabbing data in obj.list
+            maxTempIndex = findMaxTemp(futureForecastArr.slice(start, datesLastIndex[i]+1)) + datesLastIndex[i-1];
+
             maxTempArr
             .push(
-            {
+            {   // dt_txt attribute for date, returns string
                 date: moment(futureForecastArr[maxTempIndex].dt_txt).format("M/DD/YYYY"),
                 temp: futureForecastArr[maxTempIndex].main.temp,
                 wind: futureForecastArr[maxTempIndex].wind.speed,
                 humidity: futureForecastArr[maxTempIndex].main.humidity
             });
         }
-        start = dateIndex[i]+1;
+        // set start to current Index + 1, start of new subset of Array.
+        start = datesLastIndex[i]+1;
     }
     return(maxTempArr);
 };
@@ -90,10 +113,10 @@ var createCurrentForecast = function(obj, container) {
     var humEl = document.createElement("div");
     var uvEl = document.createElement("div");
 
-    headEl.textContent = cityName + " (" + currentDate + ")";
+    headEl.textContent = obj.city + " (" + currentDate + ")";
     tempEl.textContent = "Temp: " + obj.temp + " \u00B0F";
     windEl.textContent = "Wind: " + obj.wind + " MPH";
-    humEl.textContent = "Humidity: " + obj.humidity + " %";
+    humEl.textContent = "Humidity: " + obj.humidity + "%";
     uvEl.textContent = "UV Index: " + obj.uv;
 
     container.appendChild(headEl);
@@ -115,9 +138,9 @@ var createFiveDayForecast = function(arr) {
         // console.log($(this).find("#forecast"));
 
         futureDate.textContent = arr[i].date;
-        futureTemp.textContent = "Temp: " + arr[i].temp;
-        futureWind.textContent = "Wind: " + arr[i].wind;
-        futureHum.textContent = "Humidity: " + arr[i].humidity;
+        futureTemp.textContent = "Temp: " + arr[i].temp + " \u00B0F";
+        futureWind.textContent = "Wind: " + arr[i].wind + " MPH";
+        futureHum.textContent = "Humidity: " + arr[i].humidity + "%";
 
         $(this).find("#forecast").append(futureDate);
         $(this).find("#forecast").append(futureTemp);
@@ -128,19 +151,34 @@ var createFiveDayForecast = function(arr) {
     });
 };
 
-//add s in https when pushing to main branch
-var cityName = "Columbus"
-var url1 = "https://api.openweathermap.org/data/2.5/weather?q="+ cityName + "&appid=" + apiKey + "&units=imperial";
+var removeForecast = function() {
+    $("#current-forecast").children().each(function(){
+        $(this).remove();
+    })
+
+    $("#five-day-forecast").children().each(function(){
+        console.log($(this).find("#forecast").children().remove());
+    });
+};
+
+var startDisplayForecast = function(event) {
+    event.preventDefault();
+
+    removeForecast();
+
+    var cityName = cityTextEl.value;
+
+    // url to get current weather data
+    var url1 = "https://api.openweathermap.org/data/2.5/weather?q="+ cityName + "&appid=" + apiKey + "&units=imperial";
 
 
-fetch(url1)
-.then(response => response.json())
-.then(data => {
+    fetch(url1)
+    .then(response => response.json())
+    .then(data => {
         // latitude and longitude for later usage
         var lat = data.coord.lat;
         var lon = data.coord.lon;
     
-        // object to store current weather attributes
          currentWeather = {
             city: cityName,
             temp: data.main.temp,
@@ -159,17 +197,20 @@ fetch(url1)
             createCurrentForecast(currentWeather, currentForecastEl);
         });
 
-    // url for 5 day forecast
-    var url3 = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey;
-    
-    // get 5 day forecast
-    fetch(url3)
-    .then(response => response.json())
-    .then(data => {
-        let fiveDayForecast = parseForecast(data);
-        // console.log(fiveDayForecastEl.children);
+        // url for 5 day forecast
+        var url3 = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey + "&units=imperial";
 
-        // Update 5-day forecast cards
-        createFiveDayForecast(fiveDayForecast);
+        // get 5 day forecast
+        fetch(url3)
+        .then(response => response.json())
+        .then(data => {
+            let fiveDayForecast = parseForecast(data);
+
+            // Update 5-day forecast cards
+            createFiveDayForecast(fiveDayForecast);
+        });
     });
-});
+};
+
+// Add Event Listener to form submits
+searchButtonEl.addEventListener("submit", startDisplayForecast);
